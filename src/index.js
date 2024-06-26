f769dd570254178e21012c4310345b03b064c140
JacklinPrempeh <JackieP6132@gmail.com>
2024-06-26 15:23:25 +0000

    adding all files

---
+++ src/index.js
@@ -0,0 +1,4 @@
+let name = prompt('What is your name?');
+let age = prompt('What is your age');
+let email =prompt('what is your email');
+alert(`Hi${name} You are ${age}old and we will keep in touch via ${email}`);
