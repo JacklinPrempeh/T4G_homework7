f9dced37a1959ee2398d82ee2d8c14e058e550c2
JacklinPrempeh <JackieP6132@gmail.com>
2024-07-01 10:56:46 +0000

    made changes

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
 let name = prompt('What is your name?');
 let age = prompt('What is your age');
 let email =prompt('what is your email');
-alert(`Hi${name} You are ${age}old and we will keep in touch via ${email}`);
+alert(`Hi${name} You are ${age} years old and we will keep in touch via ${email}`);
